b69b4707aff82e27d49e2676e2c82c27c63ae9e2
jiyuujin <jiyuujinunite@gmail.com>
2022-02-02 21:41:45 +0900

    Tailwind CSS configuration

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <div class="title">
-      <img src="~/assets/nuxt_logo.svg" alt="Nuxt logo" class="logo" />
-      <p class="title_text">{{ `Nuxt 3` }}</p>
+    <div
+      class="flex flex-col item-center justify-center align-middle min-h-screen m-2"
+    >
+      <img src="~/assets/nuxt_logo.svg" alt="Nuxt logo" class="h-8 pr-2" />
+      <p class="p-0 m-0 text-3xl">{{ `Nuxt 3` }}</p>
     </div>
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: 'jit',
+
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+
+  content: ['./**/*.vue'],
+}
