0b0ee4ac2884559bc50ff2e8cc4d5fcb32940540
jiyuujin <jiyuujinunite@gmail.com>
2022-02-02 20:27:06 +0900

    add srcDir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  srcDir: 'app/',
   serverMiddleware: [
     { path: '/api/hello', handler: '~/server/api/hello.ts'},
   ]
