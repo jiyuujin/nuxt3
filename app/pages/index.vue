5f70a0768c8837b00bfff918cb339ce6d554e1ba
jiyuujin <jiyuujinunite@gmail.com>
2022-02-02 21:14:17 +0900

    Nuxt logo initialize

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    {{ `index page` }}
+    <div class="title">
+      <img src="~/assets/nuxt_logo.svg" alt="Nuxt logo" class="logo" />
+      <p class="title_text">{{ `Nuxt 3` }}</p>
+    </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,9 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   srcDir: 'app/',
+  css: ['~/assets/main.css'],
   serverMiddleware: [
     { path: '/api/hello', handler: '~/server/api/hello.ts'},
-  ]
+  ],
+  buildModules: ['@nuxtjs/svg']
 })
